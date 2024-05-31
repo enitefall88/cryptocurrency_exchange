40ba7a1fd8439be015a4c660f2ab70be945bf93e
enitefall <enitefall81@gmail.com>
2024-05-31 14:13:26 -0700

    add handlers

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
+from http_client import CMCHTTPClient
+from config import settings
 
 app = FastAPI()
 
+cmc_client = CMCHTTPClient(
+    base_url='https://pro-api.coinmarketcap.com',
+    api_key=settings
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+@app.get("/cryptocurrencies")
+async def  get_cryptocurrencies():
+    return await cmc_client.get_listings()
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/cryptocurrencies/{currency_id}/")
+async def  get_cryptocurrencies(currency_id: int):
+    return await cmc_client.get_listings
