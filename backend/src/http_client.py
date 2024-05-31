5c22f7e476fcfc3c326d526c8c984def40f10b59
enitefall <enitefall81@gmail.com>
2024-05-31 14:00:50 -0700

    add http client

---
+++ backend/src/http_client.py
@@ -0,0 +1,28 @@
+from aiohttp import ClientSession
+
+
+class HTTPClient:
+    def __init__(self, base_url: str, api_key: str):
+        self._session = ClientSession(
+            base_url=base_url,
+            headers={
+                'X-CMC_PRO_API_KEY': api_key,
+            }
+        )
+
+
+class CMCHTTPClient(HTTPClient):
+
+    async def get_listings(self):
+        async with self._session.get("/v1/cryptocurrency/listings/latest") as resp:
+            result = await resp.json()
+            return result["data"]
+
+
+    async def get_currency(self, currency_id: int):
+        async with self._session.get(
+            "/v2/cryptocurrency/quotes/latest",
+            params={"id": currency_id}
+        ) as resp:
+            result = await resp.json()
+            return result["data"][str(currency_id)]
