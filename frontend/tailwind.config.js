65450f76762344b54977495b2ac8de5f8f5f3ca5
enitefall <enitefall81@gmail.com>
2024-05-31 15:38:13 -0700

    fixes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    preflight: false // <== disable this!
+  },
 }
